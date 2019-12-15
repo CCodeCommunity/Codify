f4d28b9faffe2c596ee5192e56d3842201d169d9
Sandu Victor <vyctor661@gmail.com>
2019-12-15 18:49:42 +0200

    added a test command for database

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,20 @@ import { Adapter, CommandGroupBuilder } from "@enitoni/gears-discordjs";
 import { matchPrefixes, Bot } from "@enitoni/gears";
 
 import { parseArguments } from "./common/parsing/middleware/parseArguments";
-import { prefix , port, app } from "./modules/constants";
-
-
+import { prefix, port, app } from "./modules/constants";
 
 import say from "./modules/commands/say";
 import meme from "./modules/commands/meme";
 import joke from "./modules/commands/joke";
+import dbtest from "./modules/commands/database test";
+
 
 const adapter = new Adapter({ token: process.env.BOT_TOKEN || "" });
 
 const commands = new CommandGroupBuilder()
     .match(matchPrefixes(prefix))
     .use(parseArguments)
-    .setCommands(say,meme,joke)
+    .setCommands(say, meme, joke, dbtest)
     .done();
 
 const bot = new Bot({ adapter, commands: [commands] });
@@ -23,13 +23,16 @@ const bot = new Bot({ adapter, commands: [commands] });
 bot.on("error", err => console.log("Error ", err));
 
 const init = async (): Promise<void> => {
+
+    console.info(`Connecting to discord...`);
     await bot.start();
     console.info(`Logged in as ${bot.client.user.tag}`);
 
     await bot.client.user.setActivity(`Hello ${process.env.COMMUNITY}!`);
-    console.info(`Set the bot's activity!`);
+    console.info(`Bot activity is set up!`);
 
     console.info(`The bot is up and running!`);
 };
 
 app.listen(port, init);
+
