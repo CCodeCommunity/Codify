235f705d6a2b1f01e556277ebf1725f91ec32bd9
Sandu Victor <vyctor661@gmail.com>
2019-12-15 19:38:26 +0200

    Solved commit by mistake

--- dist/src/modules/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-exports.prefix = "cc!";
-exports.port = process.env.PORT || 5000;
-exports.app = express_1.default();
