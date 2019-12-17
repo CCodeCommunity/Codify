7eccef734ed368da2af22c023393cabe5dcce924
Sandu Victor <vyctor661@gmail.com>
2019-12-17 14:06:15 +0200

    formatted file with prettier

--- src/modules/commands/challenge.ts
+++ src/modules/commands/challenge.ts
@@ -1,7 +1,7 @@
 import { CommandBuilder } from "@enitoni/gears-discordjs";
 import { matchPrefixesStrict } from "../../common/matching/matchPrefixesStrict";
 
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
 export default new CommandBuilder()
     .match(matchPrefixesStrict("challenge"))
@@ -9,20 +9,24 @@ export default new CommandBuilder()
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
 
-        try{
-        await page.goto('https://seblague.github.io/ideagenerator/');
-        await page.click('.button');
+        try {
+            await page.goto("https://seblague.github.io/ideagenerator/");
+            await page.click(".button");
 
-        const element = await page.$('#content');
-        const text = await page.evaluate(element => element.textContent, element);
+            const element = await page.$("#content");
+            const text = await page.evaluate(
+                element => element.textContent,
+                element
+            );
 
-        return context.message.channel.send(`**You have to make:** ${text}`);
-        }catch(e){
-            console.info(e); 
+            return context.message.channel.send(
+                `**You have to make:** ${text}`
+            );
+        } catch (e) {
+            console.info(e);
             return context.message.channel.send(`
             **ERROR:** Could not load source page.
-            `)
+            `);
         }
-        
     })
     .done();
