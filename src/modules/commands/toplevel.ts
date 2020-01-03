499a1f65b40dfd97dd6d4dbad895d27c0fb78c51
Sandu Victor <vyctor661@gmail.com>
2020-01-03 22:06:53 +0200

    changed top level format

--- src/modules/commands/toplevel.ts
+++ src/modules/commands/toplevel.ts
@@ -4,19 +4,22 @@ import { matchPrefixesStrict } from "../../common/matching/matchPrefixesStrict";
 
 import knex from "../../../knexfile";
 
-async function fillFields() {
+async function generateTop(message: any) {
     const top = await knex("user")
         .orderBy("level", "desc")
         .orderBy("xp", "desc");
-    let fill = new Array(9);
+    let fill = "```css\n";
 
     for (let i = 0; i <= 9; i++) {
-        fill[i] = {
-            name: `\`TOP ${i + 1}\``,
-            value: `<@${top[i].userid}>\n---> **Level: ${top[i].level}** and **${top[i].xp} xp**`,
-            inline: true
-        };
+        const memberName = await message.guild.members
+            .get(`${top[i].userid}`)
+            ?.displayName.replace(/[^\w\s]|\s+/gi, "");
+        const k = i + 1 == 10 ? "10" : `0${i + 1}`;
+
+        fill += `[${k}]  #${memberName} \n`;
+        fill += `      Level ${top[i].level}  :${top[i].xp}XP \n\n`;
     }
+    fill += "```";
 
     return fill;
 }
@@ -25,13 +28,7 @@ export default new CommandBuilder()
     .match(matchPrefixesStrict("top|toplevel|topl"))
     .use(async context => {
         const { message } = context;
-        const fields = await fillFields();
-        return message.channel.send({
-            embed: {
-                description: "***TOP USERS BY LEVEL***",
-                color: 3447003,
-                fields
-            }
-        });
+        const list = await generateTop(message);
+        return message.channel.send(list);
     })
     .done();
