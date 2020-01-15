c24eb70bc540c12d5fb46123a98231babaea77b8
Sandu Victor <vyctor661@gmail.com>
2020-01-15 19:35:32 +0200

    list of levelup messages

---
+++ src/common/levelUpMessages.ts
@@ -0,0 +1,22 @@
+const messages = [
+    `Lol ok.`,
+    `Imagine that.`,
+    `Ye dude I agree.`,
+    `Nah no way.`,
+    `Nice, but we need more level up messages.`,
+    `A random potato appeared.`,
+    `Soo, you really like leveling up huh?`,
+    `Hey look you leveled up, AMAZING.`,
+    `Don't forget to check the edge cases.`,
+    `Don't forget to drink water, stay hydrated.`,
+    `PHP is the best!`,
+    `Everyone will get bored of those messages eventually so I guess I'll have to add more.`,
+    `Wait a second something is happening.`,
+    `Toss a coin to this guy, oh valley of coding.`,
+    `Imagine saying that.`,
+    `2b || !2b, it's funny cuz it's true.`
+]
+
+
+
+export const randomMessage = messages[Math.floor(Math.random() * messages.length)];
