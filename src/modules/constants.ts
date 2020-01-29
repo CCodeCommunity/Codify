c9d9690b7f3bc4a50301c9a4a925afccfa268766
Sandu Victor <vyctor661@gmail.com>
2020-01-29 16:34:15 +0200

    added new constants used by the poll command

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -4,3 +4,11 @@ export const prefix = "cc!";
 export const port = process.env.PORT || 5000;
 
 export const app = express();
+
+export const resolveArrayToOne = <T>(arr: T[] | T) => {
+    if (Array.isArray(arr)) return arr[0]
+    return arr
+}
+
+export const alphabet = "abcdefghijklmnopqrstuvwxyz"
+export const emojiLetters = ["🇦", "🇧", "🇨", "🇩", "🇪", "🇫", "🇬", "🇭", "🇮", "🇯", "🇰", "🇱", "🇲", "🇳", "🇴", "🇵", "🇶", "🇷", "🇸", "🇹", "🇺", "🇻", "🇼", "🇽", "🇾", "🇿"]
