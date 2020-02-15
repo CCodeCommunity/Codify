255d4c162f09c360bd896938d5ceeddb82e2f410
Sandu Victor <vyctor661@gmail.com>
2020-02-15 18:34:12 +0200

    fix: typo

--- src/modules/commands/token.ts
+++ src/modules/commands/token.ts
@@ -19,6 +19,6 @@ export default new CommandBuilder()
             });
 
         return message.author.send(`
-        Your token is: \`${token}\`, make sure **nobody** else knows this token because with it they will be able to access your Codify profile through the API as if they were you.\n\nTo bet using the API, send a POST request to \`https://codify-api.herokuapp.com/api/bet/\`.\nThe POST request body should look like this: \n\`\`\`json\n{\n    "id": ${message.author.id},\n    "token": ${token},\n    "amount": 42\n}\n\`\`\`\n\nIf your token gets compromised, use \`cc!gettoken\` again to regenerate the token.\nBest of luck! :grinning:`);
+        Your token is: \`${token}\`, make sure **nobody** else knows this token because with it they will be able to access your Codify profile through the API as if they were you.\n\nTo bet using the API, send a POST request to \`https://codify-api.herokuapp.com/api/bet/\`.\nThe POST request body should look like this: \n\`\`\`json\n{\n    "id": "${message.author.id}",\n    "token": "${token}",\n    "amount": 42\n}\n\`\`\`\n\nIf your token gets compromised, use \`cc!gettoken\` again to regenerate the token.\nBest of luck! :grinning:`);
     })
     .done();
