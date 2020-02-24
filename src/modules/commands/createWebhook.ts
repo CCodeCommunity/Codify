cbbbe1a9b2bb6388cf7e8bf76ca334dc282c84a9
Sandu Victor <vyctor661@gmail.com>
2020-02-24 19:34:25 +0200

    added error handeling for the comand

--- src/modules/commands/createWebhook.ts
+++ src/modules/commands/createWebhook.ts
@@ -7,11 +7,15 @@ export default new CommandBuilder()
     .match(matchPrefixesStrict("webhook|createwebhook"))
     .use(async context => {
         const { message } = context as any;
-
-        const webhook = await message.channel.createWebhook(message.author.username, message.author.displayAvatarURL)
-        if (message.member.roles.some((role: any) => role.name === "githubHooker"))
-            message.author.send(`Here is your webhook: \`https://canary.discordapp.com/api/webhooks/${webhook.id}/${webhook.token}/gihub\`, you can use it in github in any repository. \`Do not abuse!\``)
-        else
-            return message.channel.send("You do not have the right permissions for using this command!")
+        try {
+            const webhook = await message.channel.createWebhook(message.author.username, message.author.displayAvatarURL)
+            if (message.member.roles.some((role: any) => role.name === "githubHooker"))
+                message.author.send(`Here is your webhook: \`https://canary.discordapp.com/api/webhooks/${webhook.id}/${webhook.token}/gihub\`, you can use it in github in any repository. \`Do not abuse!\``)
+            else
+                return message.channel.send("You do not have the right permissions for using this command!")
+        } catch (e) {
+            console.log(e);
+            return message.channel.send("500: Internal server error.")
+        }
     })
     .done();
