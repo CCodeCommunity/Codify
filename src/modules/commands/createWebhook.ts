d9aa91757a703d50fc352ecec03447f309672567
Sandu Victor <vyctor661@gmail.com>
2020-02-24 19:47:38 +0200

    fixed typo, wrote gihub instead of github

--- src/modules/commands/createWebhook.ts
+++ src/modules/commands/createWebhook.ts
@@ -11,7 +11,7 @@ export default new CommandBuilder()
         try {
             const webhook = await message.channel.createWebhook(message.author.username, message.author.displayAvatarURL)
             if (message.member.roles.some((role: any) => role.name === "githubHooker"))
-                message.author.send(`Here is your webhook: \`https://canary.discordapp.com/api/webhooks/${webhook.id}/${webhook.token}/gihub\`, you can use it in github in any repository. \`Do not abuse!\``)
+                message.author.send(`Here is your webhook: \`https://canary.discordapp.com/api/webhooks/${webhook.id}/${webhook.token}/github\`, you can use it in github in any repository. \`Do not abuse!\``)
             else
                 return message.channel.send("You do not have the right permissions for using this command!")
         } catch (e) {
