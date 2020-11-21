7fa01c86c09779dd9a0080ec18c3a94d2d97878a
Sandu Victor <vyctor661@gmail.com>
2020-11-21 14:47:16 +0200

    fix: made codify's responses less anoying

--- src/modules/reactions/dang.ts
+++ src/modules/reactions/dang.ts
@@ -3,9 +3,9 @@ import { Message } from "discord.js";
 export default (ctx: Message) => {
     const message = ctx.content;
 
-    const responses = ["danggggg", "bruh", "not worthy of my dang", "[1]"];
+    const responses = ["danggggg", "dangg", "dangggggggggggggggggg"];
 
-    if (message.startsWith("dang") || message.includes(" dang"))
+    if (message.startsWith("dang"))
         ctx.channel.send(
             responses[Math.floor(Math.random() * responses.length)]
         );
