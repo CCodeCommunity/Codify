bc010647f45c7368cf9b7ba269d86e2337b5aae5
Sandu Victor <vyctor661@gmail.com>
2021-10-10 15:04:48 +0300

    fix: fixed a bug in the command cooldowns which made some commands share the same cooldown for some reason, now each command has its own cooldown (and it is kept track of each command cooldown separately)

--- src/common/cooldown/middleware/comandCooldown.ts
+++ src/common/cooldown/middleware/comandCooldown.ts
@@ -9,38 +9,37 @@ export const setCooldown = (timeout: number): Middleware<Cooldown> => (
     next
 ) => {
     const { message, state } = context;
+    const { content } = message;
+    const command = content.split(" ")[0];
+
     const mostRecentTime = new Date().getTime();
 
+    const key = `${message.author.id}${command}`;
     // state.lastTalkedAt = lastUsedAt;
     state.lastTalkedAt = mostRecentTime;
-    if (recentlySentCommand.has(message.author.id)) {
+    if (recentlySentCommand.has(key)) {
         if (
-            recentlySentCommand.get(message.author.id)!.lastTalkedAt + timeout >
+            recentlySentCommand.get(key)!.lastTalkedAt + timeout >
             mostRecentTime
         ) {
-            state.lastTalkedAt = recentlySentCommand.get(
-                message.author.id
-            )!.lastTalkedAt;
+            state.lastTalkedAt = recentlySentCommand.get(key)!.lastTalkedAt;
             if (message.channel.type !== "dm")
                 message.delete({ timeout: 1000 });
 
             return message.channel.send(
-                `You have to wait **${(recentlySentCommand.get(
-                    message.author.id
-                )!.lastTalkedAt +
+                `You have to wait **${(recentlySentCommand.get(key)!
+                    .lastTalkedAt +
                     timeout -
                     mostRecentTime) /
                     1000}** seconds before using this command again.`
             );
         } else {
             state.lastTalkedAt = mostRecentTime;
-            recentlySentCommand.get(
-                message.author.id
-            )!.lastTalkedAt = mostRecentTime;
+            recentlySentCommand.get(key)!.lastTalkedAt = mostRecentTime;
         }
     } else {
         state.lastTalkedAt = mostRecentTime;
-        recentlySentCommand.set(message.author.id, {
+        recentlySentCommand.set(key, {
             lastTalkedAt: mostRecentTime
         });
     }
