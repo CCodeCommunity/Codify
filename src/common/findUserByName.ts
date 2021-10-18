14fddc2405a0a3f7d9b5cc34d5088325fe08b34f
BlueGhostGH <engisoftleaderoff@gmail.com>
2021-10-18 19:35:16 +0300

    refactor: clean around some commands

--- src/common/findUserByName.ts
+++ src/common/findUserByName.ts
@@ -1,11 +1,9 @@
 import { Guild } from "discord.js";
 
 export const findUserByName = async (guild: Guild, str: string) => {
-    const users = await guild.members.cache;
+    const users = guild.members.cache;
     const matchingUsers = [...users.values()].filter(l =>
-        `${l.user.username}#${l.user.discriminator}`
-            .toLowerCase()
-            .startsWith(str)
+        l.user.tag.toLowerCase().startsWith(str)
     );
     return matchingUsers;
 };
